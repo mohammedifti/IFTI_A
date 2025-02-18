020553e61e8d56cd299753df38c19d237317910e
practice <test@test.com>
2025-02-18 15:06:56 -0500

    saving code

---
+++ src/test/java/java_practiceTest_2/Lec2MathPracticeTest_1.java
@@ -0,0 +1,62 @@
+package java_practiceTest_2;
+
+public class Lec2MathPracticeTest_1 {
+	public static void main(String[] args) {
+		// add two value 5 and 10.
+		int value1=5;
+		int value2=10;
+		int add=value1+value2;
+		System.out.println(add);
+
+		//Assignment, add 3 value, business 1 =10,business 2=20,business 3=30.
+	    // how much you make using java code.
+		
+		int business1=10;
+		int business2=20;
+		int business3=30;
+		int add1=business1+business2+business3;
+		System.out.println(add1);
+		
+		// sub 2 value 10 and 5
+	    int num1=10;
+	    int num2=5;
+	    int sub=num1-num2;
+	    System.out.println(sub);
+				
+		//subtract 10 from 100 using java code.
+		int num3=10;
+		int num4=100;
+		int subt=num3-num4;
+		System.out.println(subt);
+		
+		//deference between print and println.
+		
+		// Multiple 3 values 3,6,9.5 .
+		int value3=3;
+		int value4=6;
+		double value5=9.5;
+		double multiple=value3*value4*value5;
+		System.out.println("The total balance is " + multiple);
+		
+		// div 2 value 6.4  and 2
+		double v1=6.4;
+		double v2=2;
+		double div=v1/v2;
+		System.out.println("Total result "+ div);
+		
+		// sub total amount 100 and purchase 10.5
+	    double total_amount=100;
+		double purchase=10.5;
+		double balance=total_amount-purchase;
+		System.out.println("Total balance is "+ balance);
+		
+		//Modulus value  100 , use code in java.
+		int m=100;
+		int n=33;
+		int Modulus=m%n;
+		System.out.println("The Modulus balance is " + Modulus);
+	     
+	}
+		
+
+}
